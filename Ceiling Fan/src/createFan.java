660f3277e3dade584e86a32059068880b0044605
Kareem Abulez <31733567+kareemabulez@users.noreply.github.com>
2023-05-11 11:31:38 -0400

    Update createFan.java

--- Ceiling Fan/src/createFan.java
+++ Ceiling Fan/src/createFan.java
@@ -1,68 +1,57 @@
 import java.time.LocalDate;
+import java.time.LocalTime;
 
-public class Fan{
+public class Fan {
     int fanSpeed;
+    final int OFF = 0;
     boolean directionReversed;
     LocalDate todaysDate;
     LocalDate christmasDate;
-
-
+    
     //Class for fan
-    public Fan(){
-    //initializations for fan speed and direction of fan
-        fanSpeed = 0; //Fan speed initialized to be off
+    public Fan() {
+        //initializations for fan speed and direction of fan
+        fanSpeed = OFF; //Fan speed initialized to be off
         directionReversed = false; //direction of fan initially as normal forward
         todaysDate = LocalDate.now();
         christmasDate = LocalDate.of(todaysDate.getYear(), 12, 25);
     }
-    
+
     //method for pulling cord to increase speed
     //also checks the speed of the fan to turn it off after speed 3
-    public void increaseFanSpeed(){
-        //Checks first if today is not Christmas then
-        //After speed 3 fan turns off else increments
-        if((todaysDate.equals(christmasDate))){
+    public void increaseFanSpeed() {
+        // Check if today is Christmas eve
+        if (todaysDate.equals(christmasDate.minusDays(1))) {
+            // Keep the fan running till 11:59 PM of Christmas Eve then shuts off the fan when Christmas starts
+            if (LocalTime.now().isBefore(LocalTime.of(23, 59))) {
+                fanSpeed = (fanSpeed == 3 ? fanSpeed = OFF : fanSpeed + 1);
+                System.out.println("\nSpeed is set at: " + fanSpeed + "\n"); //Checks fan speed
+            } else {
+                System.out.println("Fans are turning off as it's Christmas day now!");
+                fanSpeed = OFF;
+            }
+        } 
+        // Check if today is Christmas day
+        else if (todaysDate.equals(christmasDate)) {
             System.out.println("Fans are off today is Christmas!!");
-        }
+        } 
+        // For other days
         else {
-            fanSpeed = (fanSpeed == 3 ? fanSpeed = 0 : fanSpeed+1);
+            fanSpeed = (fanSpeed == 3 ? fanSpeed = OFF : fanSpeed + 1);
             System.out.println("\nSpeed is set at: " + fanSpeed + "\n"); //Checks fan speed
         }
     }
-    
+
     //To reverse fan direction
-    public void reverseFan(){
-        if(fanSpeed != 0){ //as long as the fan is running
+    public void reverseFan() {
+        if (fanSpeed != OFF) { //as long as the fan is running
             directionReversed = !directionReversed; //reversing direction
         }
     }
-    
+
     //To check the direction of the fan
-    public void directionStatus(){
-        System.out.println(fanSpeed == 0 ? "\nFan is currently off" 
-        : ("\nDirection is: " + (directionReversed ? "backwards" : "forward") + "\n"));
-    }
-    
-    
-    public static void main(String[]args){
-        Fan test = new Fan();
-        //Testing fan's functionality
-        test.increaseFanSpeed(); //pulling cord and speed is 1
-        test.increaseFanSpeed(); //pulling cord and speed is 2
-        test.increaseFanSpeed(); //pulling cord and speed is 3
-        //Reversing Fan to backwards
-        test.reverseFan(); //reversing fan direction
-        test.directionStatus(); //checking the fan's direction changed after reversing
-        test.increaseFanSpeed(); //pulling cord and speed is 0 after it reached 3 and its turned off
-        test.increaseFanSpeed(); //pulling cord and speed is 1
-        //Reversing Fan to forward
-        test.reverseFan();
-        test.directionStatus();
-        
-        //Checking again if the fan will shut off after 3
-        test.increaseFanSpeed();
-        test.increaseFanSpeed();
-        test.increaseFanSpeed();
-        test.directionStatus();
+    public void directionStatus() {
+        System.out.println(fanSpeed == 0 ? "\nFan is currently off"
+                : ("\nDirection is: " + (directionReversed ? "backwards" : "forward") + "\n"));
     }
 }
